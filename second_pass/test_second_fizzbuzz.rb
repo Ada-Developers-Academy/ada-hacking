28c63ebcd84591d30a73958e0365531c518b00d2
Sam Livingston-Gray <geeksam@gmail.com>
2013-11-18 11:54:32 -0800

    extract assert_equality method

--- second_pass/test_second_fizzbuzz.rb
+++ second_pass/test_second_fizzbuzz.rb
@@ -6,21 +6,26 @@ def assert(condition, failure_message)
   end
 end
 
+def assert_equality(expected, actual)
+  failure_message = "FAILURE:  Expected #{expected.inspect}, but got #{actual.inspect}" 
+  condition = (actual == expected)
+  assert( condition, failure_message )
+end
+
 # Test 1
 actual = fizzbuzz_of(1)
 expected = 1
-condition = (actual == expected)
-assert( condition, "FAILURE:  Expected #{expected.inspect}, but got #{actual.inspect}" )
+assert_equality expected, actual
 
 # Test 2
 actual = fizzbuzz_of(2)
 expected = 2
-assert( actual == expected, "FAILURE:  Expected #{expected.inspect}, but got #{actual.inspect}" )
+assert_equality expected, actual
 
 # Test 3
 actual = fizzbuzz_of(3)
 expected = 'Fizz'
-assert( actual == expected, "FAILURE:  Expected #{expected.inspect}, but got #{actual.inspect}" )
+assert_equality expected, actual
 
 # If we got here, everything passed
 puts "SUCCESS!"
