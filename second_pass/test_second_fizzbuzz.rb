95714fe4f3be5671dccac016f96c3ddaf8413b10
Sam Livingston-Gray <geeksam@gmail.com>
2013-11-18 11:18:40 -0800

    make tests slightly shorter

--- second_pass/test_second_fizzbuzz.rb
+++ second_pass/test_second_fizzbuzz.rb
@@ -1,18 +1,18 @@
 require_relative 'fizzbuzz_function'
 
+# Test 1
 actual = fizzbuzz_of(1)
 expected = 1
-if actual == expected
-  puts "Success!"
-else
-  puts "FAILURE:  Expected 1, but got #{actual.inspect}"
+if actual != expected
+  raise "FAILURE:  Expected 1, but got #{actual.inspect}"
 end
 
+# Test 2
 actual = fizzbuzz_of(2)
 expected = 2
-if actual == expected
-  puts "Success!"
-else
-  puts "FAILURE:  Expected #{expected.inspect}, but got #{actual.inspect}"
+if actual != expected
+  raise "FAILURE:  Expected #{expected.inspect}, but got #{actual.inspect}"
 end
 
+# If we got here, everything passed
+puts "SUCCESS!"
