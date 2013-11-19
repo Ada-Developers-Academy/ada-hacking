3ca5a2faaf329c14bbf6d3dbb1ac21f0f180b94f
Sam Livingston-Gray <geeksam@gmail.com>
2013-11-18 21:51:40 -0800

    reword and reorganize specs a bit

--- poker-dice/spec/dice_spec.rb
+++ poker-dice/spec/dice_spec.rb
@@ -5,37 +5,40 @@ describe PokerDice::Die do
     PokerDice::Die.new(numeric_value)
   end
 
-  RSpec::Matchers.define :have_face_value_of do |expected|
-    match do |actual|
-      actual.to_s == expected
+  describe "face values" do
+    RSpec::Matchers.define :have_face_value_of do |expected|
+      match do |actual|
+        actual.to_s == expected
+      end
+
+      failure_message_for_should do |actual|
+        "expected that #{actual.inspect} would have a face value of #{expected}"
+      end
     end
 
-    failure_message_for_should do |actual|
-      "expected that #{actual.inspect} would have a face value of #{expected}"
+    it "should be 9 when its numeric_value is 1" do
+      expect( die(1) ).to have_face_value_of('9')
     end
-  end
 
-  it "should have a face value of 9 when its numeric value is 1" do
-    expect( die(1) ).to have_face_value_of('9')
-  end
+    it "should be 10 when its numeric_value is 2" do
+      expect( die(2) ).to have_face_value_of('10')
+    end
 
-  it "should have a face value of 10 when its numeric value is 2" do
-    expect( die(2) ).to have_face_value_of('10')
-  end
+    it "should be J when its numeric_value is 3" do
+      expect( die(3) ).to have_face_value_of('J')
+    end
 
-  it "should have a face value of J when its numeric value is 3" do
-    expect( die(3) ).to have_face_value_of('J')
-  end
+    it "should be Q when its numeric_value is 4" do
+      expect( die(4) ).to have_face_value_of('Q')
+    end
 
-  it "should have a face value of Q when its numeric value is 4" do
-    expect( die(4) ).to have_face_value_of('Q')
-  end
+    it "should be K when its numeric_value is 5" do
+      expect( die(5) ).to have_face_value_of('K')
+    end
 
-  it "should have a face value of K when its numeric value is 5" do
-    expect( die(5) ).to have_face_value_of('K')
-  end
+    it "should be A when its numeric_value is 6" do
+      expect( die(6) ).to have_face_value_of('A')
+    end
 
-  it "should have a face value of A when its numeric value is 6" do
-    expect( die(6) ).to have_face_value_of('A')
   end
 end
