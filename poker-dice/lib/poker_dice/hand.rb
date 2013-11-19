9a851ec0c6996e1a722d5242ba34133bc6af4848
Sam Livingston-Gray <geeksam@gmail.com>
2013-11-18 22:51:15 -0800

    Extract hand classification to a new object

--- poker-dice/lib/poker_dice/hand.rb
+++ poker-dice/lib/poker_dice/hand.rb
@@ -8,25 +8,39 @@ module PokerDice
     end
 
     def description
-      case
-      when counts.values == [5]
-        "Five of a kind"
-      when counts.values.sort == [1, 4]
-        "Four of a kind"
-      when counts.values.sort == [2, 3]
-        "Full house"
-      when dice.map(&:numeric_value).sort == (1..5).to_a ||
-           dice.map(&:numeric_value).sort == (2..6).to_a
-        "Straight"
-      end
+      Classifier.new(dice).description
     end
 
-    def counts
-      counts = Hash.new {|h,k| h[k] = 0 }
-      dice.each do |die|
-        counts[die.face_value] += 1
+    class Classifier
+      attr_reader :dice
+
+      def initialize(dice)
+        @dice = dice
       end
-      counts
+
+      def description
+        case
+        when counts.values == [5]
+          "Five of a kind"
+        when counts.values.sort == [1, 4]
+          "Four of a kind"
+        when counts.values.sort == [2, 3]
+          "Full house"
+        when dice.map(&:numeric_value).sort == (1..5).to_a ||
+             dice.map(&:numeric_value).sort == (2..6).to_a
+          "Straight"
+        end
+      end
+
+      private
+
+      def counts
+        counts = Hash.new {|h,k| h[k] = 0 }
+        dice.each do |die|
+          counts[die.face_value] += 1
+        end
+        counts
+      end
     end
   end
 end
