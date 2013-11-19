27321bd2a0111ef9d8d6e5a12330b3ad37401141
Sam Livingston-Gray <geeksam@gmail.com>
2013-11-18 21:20:30 -0800

    Move local variables to private accessors

--- rspec-exercise/calculator.rb
+++ rspec-exercise/calculator.rb
@@ -23,32 +23,41 @@ class Calculator
   class Evaluator
     attr_reader :expression
     def initialize(expression)
-      @expression = expression
+      @expression  = expression
+      @accumulator = nil
+      @value       = nil
+      @operator    = nil
     end
 
+    attr_accessor :accumulator, :value, :operator
+    private :accumulator, :value, :operator
+
     def call
-      accumulator = nil
-      value, operator = nil, nil
-      tokens = Tokenizer.new(expression)
       until tokens.empty?
         case tokens.first
         when /\d+/          # Integer
-          value = tokens.consume.to_i
+          self.value = tokens.consume.to_i
           case
           when accumulator.nil? # should only be the first time through
-            accumulator = value
+            self.accumulator = value
           when operator.nil?
             raise "I don't understand #{expression.inspect}"
           else
-            accumulator = accumulator.send(operator, value)
+            self.accumulator = accumulator.send(operator, value)
           end
         when /[\+\-\*\/]/   # Operator
-          operator = tokens.consume.to_sym
+          self.operator = tokens.consume.to_sym
         else
           raise "I don't understand #{expression.inspect}"
         end
       end
       accumulator
+    end
+
+    private
+
+    def tokens
+      @tokens ||= Tokenizer.new(expression)
     end
   end
 
