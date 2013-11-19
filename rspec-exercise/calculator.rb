9e12054aae9688a04bd78127999241166579f822
Sam Livingston-Gray <geeksam@gmail.com>
2013-11-18 21:02:00 -0800

    horrible tokenization/evaluation thing

--- rspec-exercise/calculator.rb
+++ rspec-exercise/calculator.rb
@@ -14,4 +14,29 @@ class Calculator
   def divide(*numbers)
     numbers.inject(numbers.shift, &:/)
   end
+
+  def evaluate(expression)
+    accumulator = nil
+    value, operator = nil, nil
+    tokens = expression.split(/\b/)
+    until tokens.empty?
+      case tokens.first
+      when /\d+/          # Integer
+        value = tokens.shift.to_i  # consume the token, then...
+        case
+        when accumulator.nil? # should only be the first time through
+          accumulator = value
+        when operator.nil?
+          raise "I don't understand #{expression.inspect}"
+        else
+          accumulator = accumulator.send(operator, value)
+        end
+      when /[\+\-\*\/]/   # Operator
+        operator = tokens.shift.to_sym # consume the token
+      else
+        raise "I don't understand #{expression.inspect}"
+      end
+    end
+    accumulator
+  end
 end
