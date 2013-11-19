3bc9ad3a38fc204c8e9b115b4c1d1c4a24de53ad
Sam Livingston-Gray <geeksam@gmail.com>
2013-11-18 21:47:35 -0800

    Use a custom matcher for checking face value

--- poker-dice/spec/dice_spec.rb
+++ poker-dice/spec/dice_spec.rb
@@ -5,11 +5,21 @@ describe PokerDice::Die do
     PokerDice::Die.new(numeric_value)
   end
 
+  RSpec::Matchers.define :have_face_value_of do |expected|
+    match do |actual|
+      actual.to_s == expected
+    end
+
+    failure_message_for_should do |actual|
+      "expected that #{actual.inspect} would have a face value of #{expected}"
+    end
+  end
+
   it "should have a face value of 9 when its numeric value is 1" do
-    expect( die(1).to_s ).to eq('9')
+    expect( die(1) ).to have_face_value_of('9')
   end
 
   it "should have a face value of 10 when its numeric value is 2" do
-    expect( die(2).to_s ).to eq('10')
+    expect( die(2) ).to have_face_value_of('10')
   end
 end
